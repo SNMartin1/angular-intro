12e9fcc0fef899d42aafd316aa9a38bf24c3df65
Nicole Martin <nikismar@hotmail.com>
2017-07-14 15:21:21 -0500

    base mode done

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # angular-intro
+
+[x]- on view, need one input field that takes a user name.
+[x]- need a second input field that takes a message from a user.
+[]- create a submit button and click event for the button, so that on click of the button, the text
+is taken from the input fields and then appears on the DOM.
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,15 @@
 console.log('JS sourced');
+
+//created app and linked angular to it
+var myApp = angular.module('myApp', []);
+
+myApp.controller('OnloadController', function() {
+  var control = this;
+  console.log(this);
+
+
+  control.displayInfo = function(userName, userMessage) {
+    var info = {userName: userName, userMessage: userMessage};
+    console.log(info);
+  };
+});
